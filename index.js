c4768994ea0a42b1b82171280bdcf43d34365208
Amlan Saikia <83667291+TheAmlanSaikia@users.noreply.github.com>
2021-09-01 08:42:20 +0000

    Completed 5 questions Checklist

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var readlineSync = require('readline-sync');
 
 var score = 0;
 var username = readlineSync.question("What's your name? ");
-console.log('Welcome '+ username+ 'How much do You Know Amlan?');
+console.log('Welcome '+ username  + ' How much do You Know Amlan?');
 
 function play(question,answer) {
     var useranswer = readlineSync.question(question);
@@ -29,6 +29,14 @@ var questions = [
     {
         question:"My favorite superhero would be? ",
         answer: "Batman"
+    },
+     {
+        question:"My favorite Movie? ",
+        answer: "Batman: The Dark Knight Rises"
+    },
+     {
+        question:"My favorite food would be? ",
+        answer: "Dumplings"
     },
 ];
 
